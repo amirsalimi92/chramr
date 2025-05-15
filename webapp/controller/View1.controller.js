8b66ed2f2a4d0bd58ddbf77e9633c4af3acbf670
Amir Salimi <amir.salimi1810@gmail.com>
2025-05-15 11:37:50 +0200

    first

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,20 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+sap.ui.define(['sap/ui/core/mvc/Controller'], Controller => {
+  'use strict'
 
-    return Controller.extend("de.cpro.schulung.chramr.controller.View1", {
-        onInit() {
-        }
-    });
-});
+  return Controller.extend('de.cpro.schulung.chramr.controller.View1', {
+    onInit () {},
+
+    onChange (event) {
+      console.log(event)
+      const item = event.getSource()
+      const oParent = item.getParent()
+      console.log({ oParent })
+      const oContext = oParent.getBindingContext()
+      console.log(oContext)
+    },
+
+    onPress (event) {
+      console.log(event.getSource())
+    }
+  })
+})
